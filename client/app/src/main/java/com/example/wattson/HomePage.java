e4ba6e8bcd80591b1400b30274fed08d6237b517
Shai Brown <shaibrown95@gmail.com>
2021-04-16 12:38:41 +0300

    finished navigation bar of home page and made fragments

--- client/app/src/main/java/com/example/wattson/HomePage.java
+++ client/app/src/main/java/com/example/wattson/HomePage.java
@@ -2,11 +2,16 @@ package com.example.wattson;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class HomePage extends AppCompatActivity {
 
+    MeowBottomNavigation bottomNavigation;
     private static final String TAG = "HomePage";
 
     @Override
@@ -15,6 +20,62 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i(TAG, "in home page");
+        bottomNavigation = findViewById(R.id.bottom_navigation);
 
+        //add menue items
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_home_icon));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_plug_icon));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_graph_icon));
+        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_person_icon));
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                // initialize fragments
+                Fragment fragment = null;
+
+                switch (item.getId()){
+                    case 1:
+                        fragment = new HomeFragment();
+                        break;
+                    case 2:
+                        fragment = new TodaysActivityFragment();
+                        break;
+                    case 3:
+                        fragment = new StatisticsFragment();
+                        break;
+                    case 4:
+                        fragment = new SettingsFragment();
+                        break;
+
+                }
+
+                loadFragment(fragment);
+            }
+        });
+        //set home fragment initially selected
+        bottomNavigation.show(1, true);
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(), "I am in " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(), "I reselected " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        //replace fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .commit();
     }
 }
