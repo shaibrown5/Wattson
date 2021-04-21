1b1222024f8b930b25e7b078ff3e143f96b3ddf9
Shai Brown <shaibrown95@gmail.com>
2021-04-21 15:49:50 +0300

    added logout button in SettingsFragment

--- client/app/src/main/java/com/example/wattson/SettingsFragment.java
+++ client/app/src/main/java/com/example/wattson/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.wattson;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.wattson.Adapter.SettingsAdapter;
 import com.example.wattson.utils.SpacingItemDecorator;
@@ -24,6 +26,7 @@ public class SettingsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Button logOutButton;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -76,6 +79,15 @@ public class SettingsFragment extends Fragment {
         SpacingItemDecorator itemDecor = new SpacingItemDecorator(50);
         rView.addItemDecoration(itemDecor);
 
+        logOutButton = (Button) rootView.findViewById(R.id.buttonLogOut);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
