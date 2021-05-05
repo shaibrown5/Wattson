db6303f2f16c6c1fde6787f8911fe2bcc1f452cf
Shai Brown <shaibrown95@gmail.com>
2021-05-05 09:34:08 +0300

    delete activity when log out

--- client/app/src/main/java/com/example/wattson/SettingsFragment.java
+++ client/app/src/main/java/com/example/wattson/SettingsFragment.java
@@ -94,6 +94,7 @@ public class SettingsFragment extends Fragment {
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), LoginActivity.class);
                 startActivity(intent);
+                getActivity().finish();
             }
         });
     }
