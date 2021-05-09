cacf99eed461256e11165f87af5a60d54497323e
Shai Brown <shaibrown95@gmail.com>
2021-05-10 00:24:40 +0300

    created onboarding pages and movement

--- client/app/src/main/java/com/example/wattson/LoginActivity.java
+++ client/app/src/main/java/com/example/wattson/LoginActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button m_logInButton;
+    private Button m_logInButton;
+    private TextView txt_signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login2);
 
         m_logInButton = (Button) findViewById(R.id.logInButton);
+        txt_signUp = (TextView) findViewById(R.id.signUpText);
 
         m_logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        txt_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), OnBoardingActivity.class);
                 startActivity(intent);
                 finish();
             }
