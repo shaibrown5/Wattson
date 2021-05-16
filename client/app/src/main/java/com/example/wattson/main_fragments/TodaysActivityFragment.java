be99d97103cc41f7472b26e69e74eec094b4fc00
Shai Brown <shaibrown95@gmail.com>
2021-05-16 16:21:12 +0300

    created adapter for activity and card

--- client/app/src/main/java/com/example/wattson/main_fragments/TodaysActivityFragment.java
+++ client/app/src/main/java/com/example/wattson/main_fragments/TodaysActivityFragment.java
@@ -2,15 +2,22 @@ package com.example.wattson.main_fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.wattson.HomeActivity;
 import com.example.wattson.R;
 
 public class TodaysActivityFragment extends Fragment {
+
+    private HomeActivity ac_HomeActivity;
+    private RecyclerView m_rView;
 
     public TodaysActivityFragment() {
         // Required empty public constructor
@@ -25,7 +32,16 @@ public class TodaysActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todays_activity, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_todays_activity, container, false);
+
+        ac_HomeActivity = (HomeActivity) getActivity();
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull  View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        m_rView = getView().findViewById(R.id.recyclerActivityLog);
     }
 }
