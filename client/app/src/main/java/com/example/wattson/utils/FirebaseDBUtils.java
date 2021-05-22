d436768208599e8a3f57d79d4170e0daeb3a2409
Shai Brown <shaibrown95@gmail.com>
2021-05-22 19:57:36 +0300

    added logic to util page for day only

--- client/app/src/main/java/com/example/wattson/utils/FirebaseDBUtils.java
+++ client/app/src/main/java/com/example/wattson/utils/FirebaseDBUtils.java
@@ -12,7 +12,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,7 +58,7 @@ public class FirebaseDBUtils {
                     }
 
                     ApplianceInfo temp = new ApplianceInfo(applianceName, individualReadingList);
-                    temp.updateDailyCost();
+                    temp.updateCostAndActivationsNum();
                     applianceInfoList.add(temp);
                 }
 
